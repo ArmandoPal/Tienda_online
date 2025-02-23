7c22be3a068a21bf01334d91b8d5b2150508f6b3
ArmandoPal <jorge.martinez@utec.edu.pe>
2025-02-23 00:07:09 -0500

    Primer commit - Subiendo el proyecto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+//import (lo que quieres importar) from 'react'
+
+import './App.css'
+import BarraNavegacion from './components/navbar/Navbar'
+
+
+function App() {
+  return(
+    <BarraNavegacion/>
+)
+}
+
+export default App
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import  "./NavbarS.css"
+
+function BarraNavegacion(){
+    return(
+        <div className="header">
+                <img src=""/>
+                <a className="contacto"> Contacto:  987654321 </a> 
+        </div>
+    )
+} 
+
+export default BarraNavegacion;
